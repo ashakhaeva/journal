f0e44bd617eebd57208dd2f3de2772d6e35c0f1e
Anastasia Shakhaeva <ashakhaeva@gmail.com>
2020-02-03 22:24:14 +0300

    some updates

--- journal.js
+++ journal.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // Check off specific todos by clicking
     var day = $("ul");
     day.on("click", ".meeting", function (){
         $(this).toggleClass("completed_meeting");
@@ -14,6 +16,17 @@ $(document).ready(function () {
         $(this).toggleClass("completed_list_item");
         $("span", this).toggleClass("green");
     });
+
+    //add new todos
+    $("div.container > input").keyup(function(event){
+        if(event.which === 13) {
+            // grab text of new todo from input
+            var todoText = $(this).val();
+            $(this).val("");
+            // create new li and add to ul
+            $(this).siblings('ul').prepend("<li class = todo ><span><i class='far fa-square'></i></span> " + todoText + "</li>");
+            // $("div.tuesday > ul").prepend("<li class = todo ><span><i class='far fa-square'></i></span> " + todoText + "</li>");
+        }});
 });
 
 
