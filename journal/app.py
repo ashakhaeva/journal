1ccc8d08506d3d85f04144a56de1372bc3069e3d
Anastasia Shakhaeva <ashakhaeva@gmail.com>
2020-03-28 18:20:14 +0300

    db init

--- journal/app.py
+++ journal/app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-
-
-
-app = Flask(__name__)
-
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, g
+from . import db
 
 email = "ash@ash"
 password = "12345"
 name = "Anastasia"
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config["MONGO_URI"] = ""
+    app.secret_key = b''
+    with app.app_context():
+        db.init_app(app)
+    return app
+
+
+app = create_app()
+
 
 @app.route('/')
 def hello_world():
@@ -16,9 +24,11 @@ def hello_world():
         return render_template('index.html', name=name)
     return render_template('index.html')
 
+
 @app.route('/another')
 def another():
     return "Another route"
+
 
 @app.route("/login", methods=("GET", "POST"))
 def login():
@@ -30,23 +40,17 @@ def login():
         if error is None:
             session.clear()
             session["name"] = name
-            return redirect ('/')
+            return redirect('/')
 
     else:
-        return redirect ('/login')
-        print("No such user")
+        return redirect('/login')
 
 
-
-
-
-
-
-
-
+@app.route('/testing_db')
+def show_users():
+    user = g.db.users.find_one({"name": "Anastasia"})
+    return jsonify({"name": user["name"]})
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
-
---
+++ journal/db.py
@@ -0,0 +1,17 @@
+from flask import g
+from flask_pymongo import PyMongo
+
+
+def get_db(app):
+    if 'db' not in g:
+        g.db = PyMongo(app)
+    return g.db
+
+
+def close_db(e=None):
+    g.pop('db', None)
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    get_db(app)
